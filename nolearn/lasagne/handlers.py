8d6523bfd3009eef38a34b877f5c027b8b5be614
mehdi <mehdi@cherti.name>
2015-10-05 14:03:46 +0200

    add early stopping

--- nolearn/lasagne/handlers.py
+++ nolearn/lasagne/handlers.py
@@ -11,6 +11,8 @@ from .util import ansi
 from .util import get_conv_infos
 from .util import is_conv2d
 
+import numpy as np
+
 
 class PrintLog:
     def __init__(self):
@@ -208,3 +210,31 @@ class PrintLayerInfo:
             )
 
         return layer_infos, legend
+
+
+class EarlyStopping(object):
+    def __init__(self, patience=100):
+        self.patience = patience
+        self.best_valid = np.inf
+        self.best_valid_epoch = 0
+        self.best_weights = None
+
+    def __call__(self, nn, train_history):
+        current_valid = train_history[-1]['valid_loss']
+        current_epoch = train_history[-1]['epoch']
+        if current_valid < self.best_valid:
+            self.best_valid = current_valid
+            self.best_valid_epoch = current_epoch
+            self.best_weights = nn.get_all_params_values()
+        elif self.best_valid_epoch + self.patience < current_epoch:
+            if nn.verbose:
+                print("Early stopping.")
+                print("Best valid loss was {:.6f} at epoch {}.".format(
+                    self.best_valid, self.best_valid_epoch))
+            nn.load_weights_from(self.best_weights)
+            if nn.verbose:
+                print("Weights set.")
+            raise StopIteration()
+
+    def load_best_weights(self, nn, train_history):
+        nn.load_weights_from(self.best_weights)
